vietairs/herdr

feat: add optional nix flake support
refs #208

refs #221

refs #264

flake.nix
@@ -1,0 +1,82 @@
+{
+  description = "herdr — terminal workspace manager for AI coding agents";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = lib.genAttrs systems;
+      pkgsFor = system: import nixpkgs { inherit system; };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+          herdr = pkgs.callPackage ./nix/package.nix { };
+        in
+        {
+          inherit herdr;
+          default = herdr;
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/herdr";
+          meta.description = "Run Herdr";
+        };
+      });
+
+      checks = forAllSystems (system: {
+        herdr = self.packages.${system}.default;
+        default = self.checks.${system}.herdr;
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            name = "herdr-dev";
+            packages = with pkgs; [
+              cargo
+              cargo-nextest
+              clippy
+              cmake
+              just
+              ninja
+              pkg-config
+              rustc
+              rustfmt
+              zig_0_15
+            ];
+
+            env = {
+              LIBGHOSTTY_VT_OPTIMIZE = "Debug";
+              LIBGHOSTTY_VT_SIMD = "true";
+            };
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: (pkgsFor system).nixfmt);
+
+      overlays.default = final: _prev: {
+        herdr = final.callPackage ./nix/package.nix { };
+      };
+    };
+}

nix/package.nix
@@ -1,0 +1,76 @@
+{
+  lib,
+  rustPlatform,
+  callPackage,
+  runCommand,
+  zig_0_15,
+  zstd,
+  pkg-config,
+  git,
+}:
+
+let
+  manifest = lib.importTOML ../Cargo.toml;
+  zigDeps = callPackage ../vendor/libghostty-vt/build.zig.zon.nix {
+    name = "herdr-libghostty-vt-zig-cache";
+    inherit zstd;
+    linkFarm = name: entries:
+      runCommand name { } ''
+        mkdir -p $out
+        ${lib.concatMapStringsSep "\n" (entry: ''
+          cp -rL ${entry.path} $out/${entry.name}
+        '') entries}
+      '';
+  };
+in
+rustPlatform.buildRustPackage {
+  pname = "herdr";
+  version = manifest.package.version;
+
+  src = lib.fileset.toSource {
+    root = ./..;
+    fileset = lib.fileset.intersection (lib.fileset.fromSource (lib.sources.cleanSource ./..)) (
+      lib.fileset.unions [
+        ../assets
+        ../src
+        ../vendor/libghostty-vt
+        ../vendor/libghostty-vt.vendor.json
+        ../build.rs
+        ../Cargo.lock
+        ../Cargo.toml
+      ]
+    );
+  };
+
+  cargoLock.lockFile = ../Cargo.lock;
+
+  nativeBuildInputs = [
+    git
+    pkg-config
+  ];
+
+  env = {
+    LIBGHOSTTY_VT_OPTIMIZE = "ReleaseFast";
+    LIBGHOSTTY_VT_SIMD = "true";
+    LIBGHOSTTY_VT_ZIG_SYSTEM_DIR = zigDeps;
+    ZIG = lib.getExe zig_0_15;
+  };
+
+  preBuild = ''
+    export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-global-cache"
+    export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-local-cache"
+  '';
+
+  # Rust tests are covered by the normal CI workflow. The Nix check is
+  # intentionally build-only so it validates packaging inputs without
+  # duplicating the full Rust test suite.
+  doCheck = false;
+
+  meta = {
+    description = "Terminal workspace manager for AI coding agents";
+    homepage = "https://herdr.dev";
+    license = lib.licenses.agpl3Plus;
+    mainProgram = "herdr";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+}

src/update.rs
@@ -21,6 +21,7 @@ const UPDATE_MANIFEST_URL: &str = "https://herdr.dev/latest.json";
 const HOMEBREW_FORMULA_API_URL: &str = "https://formulae.brew.sh/api/formula/herdr.json";
 const HERDR_UPDATE_COMMAND: &str = "herdr update";
 const HOMEBREW_UPDATE_COMMAND: &str = "brew update && brew upgrade herdr";
+const NIX_UPDATE_COMMAND: &str = "update through Nix";
 const CURRENT_VERSION: &str = env!("CARGO_PKG_VERSION");
 const FAKE_UPDATE_VERSION_ENV: &str = "HERDR_FAKE_UPDATE_VERSION";
 const FAKE_UPDATE_NOTES_VERSION_ENV: &str = "HERDR_FAKE_UPDATE_NOTES_VERSION";
@@ -678,6 +679,8 @@ fn wait_for_server_shutdown(timeout: Duration) -> Result<(), String> {
 pub(crate) fn update_install_command() -> &'static str {
     if is_homebrew_managed_install() {
         HOMEBREW_UPDATE_COMMAND
+    } else if is_nix_managed_install() {
+        NIX_UPDATE_COMMAND
     } else {
         HERDR_UPDATE_COMMAND
     }
@@ -695,6 +698,24 @@ fn is_homebrew_managed_install() -> bool {
     current_exe
         .canonicalize()
         .is_ok_and(|path| is_homebrew_managed_exe_path(&path))
+}
+
+fn is_nix_managed_install() -> bool {
+    let Ok(current_exe) = env::current_exe() else {
+        return false;
+    };
+
+    if is_nix_store_exe_path(&current_exe) {
+        return true;
+    }
+
+    current_exe
+        .canonicalize()
+        .is_ok_and(|path| is_nix_store_exe_path(&path))
+}
+
+fn is_nix_store_exe_path(path: &Path) -> bool {
+    path.starts_with("/nix/store")
 }
 
 fn is_homebrew_managed_exe_path(path: &Path) -> bool {
@@ -991,6 +1012,12 @@ pub fn self_update() -> Result<Version, String> {
         ));
     }
 
+    if is_nix_managed_install() {
+        return Err(
+            "self-update is disabled for Nix installs; update with `nix profile upgrade` or update the flake input that provides Herdr".into(),
+        );
+    }
+
     if running_inside_herdr() {
         return Err("run `herdr update` outside herdr after detaching from the session".into());
     }
@@ -1075,6 +1102,8 @@ pub fn auto_update(events: tokio::sync::mpsc::Sender<crate::events::AppEvent>) {
         return;
     }
 
+    let nix_managed_install = is_nix_managed_install();
+
     let release = match check_latest() {
         Ok(Some(r)) => r,
         Ok(None) => return,
@@ -1103,9 +1132,15 @@ pub fn auto_update(events: tokio::sync::mpsc::Sender<crate::events::AppEvent>) {
     );
 
     // Notify the TUI — blocking_send is safe from a std::thread
+    let install_command = if nix_managed_install {
+        NIX_UPDATE_COMMAND
+    } else {
+        HERDR_UPDATE_COMMAND
+    };
+
     let _ = events.blocking_send(crate::events::AppEvent::UpdateReady {
         version: release.version.to_string(),
-        install_command: HERDR_UPDATE_COMMAND.to_string(),
+        install_command: install_command.to_string(),
     });
 }
 
@@ -1285,6 +1320,20 @@ mod tests {
         let path = Path::new("/usr/local/bin/herdr");
 
         assert!(!is_homebrew_managed_exe_path(path));
+    }
+
+    #[test]
+    fn nix_store_path_is_detected() {
+        let path = Path::new("/nix/store/abc123-herdr-0.6.1/bin/herdr");
+
+        assert!(is_nix_store_exe_path(path));
+    }
+
+    #[test]
+    fn non_nix_store_path_is_not_detected() {
+        let path = Path::new("/usr/local/bin/herdr");
+
+        assert!(!is_nix_store_exe_path(path));
     }
 
     #[test]

masakirocorp/gardn

flake.nix
@@ -1,0 +1,82 @@
+{
+  description = "hako — terminal workspace manager for AI coding agents";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = lib.genAttrs systems;
+      pkgsFor = system: import nixpkgs { inherit system; };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+          hako = pkgs.callPackage ./nix/package.nix { };
+        in
+        {
+          inherit hako;
+          default = hako;
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/hako";
+          meta.description = "Run Hako";
+        };
+      });
+
+      checks = forAllSystems (system: {
+        hako = self.packages.${system}.default;
+        default = self.checks.${system}.hako;
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            name = "hako-dev";
+            packages = with pkgs; [
+              cargo
+              cargo-nextest
+              clippy
+              cmake
+              just
+              ninja
+              pkg-config
+              rustc
+              rustfmt
+              zig_0_15
+            ];
+
+            env = {
+              LIBGHOSTTY_VT_OPTIMIZE = "Debug";
+              LIBGHOSTTY_VT_SIMD = "true";
+            };
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: (pkgsFor system).nixfmt);
+
+      overlays.default = final: _prev: {
+        hako = final.callPackage ./nix/package.nix { };
+      };
+    };
+}

nix/package.nix
@@ -1,0 +1,76 @@
+{
+  lib,
+  rustPlatform,
+  callPackage,
+  runCommand,
+  zig_0_15,
+  zstd,
+  pkg-config,
+  git,
+}:
+
+let
+  manifest = lib.importTOML ../Cargo.toml;
+  zigDeps = callPackage ../vendor/libghostty-vt/build.zig.zon.nix {
+    name = "hako-libghostty-vt-zig-cache";
+    inherit zstd;
+    linkFarm = name: entries:
+      runCommand name { } ''
+        mkdir -p $out
+        ${lib.concatMapStringsSep "\n" (entry: ''
+          cp -rL ${entry.path} $out/${entry.name}
+        '') entries}
+      '';
+  };
+in
+rustPlatform.buildRustPackage {
+  pname = "hako";
+  version = manifest.package.version;
+
+  src = lib.fileset.toSource {
+    root = ./..;
+    fileset = lib.fileset.intersection (lib.fileset.fromSource (lib.sources.cleanSource ./..)) (
+      lib.fileset.unions [
+        ../assets
+        ../src
+        ../vendor/libghostty-vt
+        ../vendor/libghostty-vt.vendor.json
+        ../build.rs
+        ../Cargo.lock
+        ../Cargo.toml
+      ]
+    );
+  };
+
+  cargoLock.lockFile = ../Cargo.lock;
+
+  nativeBuildInputs = [
+    git
+    pkg-config
+  ];
+
+  env = {
+    LIBGHOSTTY_VT_OPTIMIZE = "ReleaseFast";
+    LIBGHOSTTY_VT_SIMD = "true";
+    LIBGHOSTTY_VT_ZIG_SYSTEM_DIR = zigDeps;
+    ZIG = lib.getExe zig_0_15;
+  };
+
+  preBuild = ''
+    export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-global-cache"
+    export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-local-cache"
+  '';
+
+  # Rust tests are covered by the normal CI workflow. The Nix check is
+  # intentionally build-only so it validates packaging inputs without
+  # duplicating the full Rust test suite.
+  doCheck = false;
+
+  meta = {
+    description = "Terminal workspace manager for AI coding agents";
+    homepage = "https://hako.masakiro.com";
+    license = lib.licenses.agpl3Plus;
+    mainProgram = "hako";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+}

src/update.rs
@@ -21,6 +21,7 @@ const UPDATE_MANIFEST_URL: &str = "https://hako.masakiro.com/latest.json";
 const HOMEBREW_FORMULA_API_URL: &str = "https://formulae.brew.sh/api/formula/hako.json";
 const HAKO_UPDATE_COMMAND: &str = "hako update";
 const HOMEBREW_UPDATE_COMMAND: &str = "brew update && brew upgrade hako";
+const NIX_UPDATE_COMMAND: &str = "update through Nix";
 const CURRENT_VERSION: &str = env!("CARGO_PKG_VERSION");
 const FAKE_UPDATE_VERSION_ENV: &str = "HAKO_FAKE_UPDATE_VERSION";
 const SERVER_STOP_RESPONSE_TIMEOUT: Duration = Duration::from_secs(5);
@@ -666,6 +667,8 @@ fn wait_for_server_shutdown(timeout: Duration) -> Result<(), String> {
 pub(crate) fn update_install_command() -> &'static str {
     if is_homebrew_managed_install() {
         HOMEBREW_UPDATE_COMMAND
+    } else if is_nix_managed_install() {
+        NIX_UPDATE_COMMAND
     } else {
         HAKO_UPDATE_COMMAND
     }
@@ -683,6 +686,24 @@ fn is_homebrew_managed_install() -> bool {
     current_exe
         .canonicalize()
         .is_ok_and(|path| is_homebrew_managed_exe_path(&path))
+}
+
+fn is_nix_managed_install() -> bool {
+    let Ok(current_exe) = env::current_exe() else {
+        return false;
+    };
+
+    if is_nix_store_exe_path(&current_exe) {
+        return true;
+    }
+
+    current_exe
+        .canonicalize()
+        .is_ok_and(|path| is_nix_store_exe_path(&path))
+}
+
+fn is_nix_store_exe_path(path: &Path) -> bool {
+    path.starts_with("/nix/store")
 }
 
 fn is_homebrew_managed_exe_path(path: &Path) -> bool {
@@ -979,6 +1000,12 @@ pub fn self_update() -> Result<Version, String> {
         ));
     }
 
+    if is_nix_managed_install() {
+        return Err(
+            "self-update is disabled for Nix installs; update with `nix profile upgrade` or update the flake input that provides Hako".into(),
+        );
+    }
+
     if running_inside_hako() {
         return Err("run `hako update` outside hako after detaching from the session".into());
     }
@@ -1063,6 +1090,8 @@ pub fn auto_update(events: tokio::sync::mpsc::Sender<crate::events::AppEvent>) {
         return;
     }
 
+    let nix_managed_install = is_nix_managed_install();
+
     let release = match check_latest() {
         Ok(Some(r)) => r,
         Ok(None) => return,
@@ -1091,9 +1120,15 @@ pub fn auto_update(events: tokio::sync::mpsc::Sender<crate::events::AppEvent>) {
     );
 
     // Notify the TUI — blocking_send is safe from a std::thread
+    let install_command = if nix_managed_install {
+        NIX_UPDATE_COMMAND
+    } else {
+        HAKO_UPDATE_COMMAND
+    };
+
     let _ = events.blocking_send(crate::events::AppEvent::UpdateReady {
         version: release.version.to_string(),
-        install_command: HAKO_UPDATE_COMMAND.to_string(),
+        install_command: install_command.to_string(),
     });
 }
 
@@ -1273,6 +1308,20 @@ mod tests {
         let path = Path::new("/usr/local/bin/hako");
 
         assert!(!is_homebrew_managed_exe_path(path));
+    }
+
+    #[test]
+    fn nix_store_path_is_detected() {
+        let path = Path::new("/nix/store/abc123-hako-0.6.1/bin/hako");
+
+        assert!(is_nix_store_exe_path(path));
+    }
+
+    #[test]
+    fn non_nix_store_path_is_not_detected() {
+        let path = Path::new("/usr/local/bin/hako");
+
+        assert!(!is_nix_store_exe_path(path));
     }
 
     #[test]